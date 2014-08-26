8bf849c5d4ed1a694c3b79a7ea316af3ccb1ded4
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-08-26 09:55:26 -0400

    new fixture sdks, with configs

---
+++ test/fixtures/test1.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'test1',
+	version: '0.0.1',
+	organization: 'appcelerator',
+	url: 'https://localhost:54321',
+	apis: {}
+};
---
+++ test/fixtures/test2.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'test2',
+	version: '0.3.1',
+	organization: 'appcelerator',
+	url: 'https://localhost:54321',
+	apis: {}
+};
